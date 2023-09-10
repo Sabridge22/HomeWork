74bf46a748c9108d5fa1789bdca3269d60ca5089
Sabridge22 <alegzandr2004@gmail.com>
2023-09-10 14:54:03 +0300

    Домашка

--- Work.py
+++ Work.py
@@ -1,1 +1,38 @@
-print('123')
+def lenA(n): # функция находит длину числа 
+    k = 1
+    cnt = 0
+    while k < n:
+        k *= 10
+        cnt += 1
+    return cnt
+
+def Left(n, ln): # функция находит первое число
+    n = n // (10 ** (ln - 1))
+    return n
+
+def Right(n): # функция находит последнее число
+    n = n % 10
+    return n
+
+def pos(n): # функция срезает последнее число
+    n = n // 10
+    return n
+
+def per(n, ln): # функция срезает первое число
+    n = n % (10 ** (ln-1))
+    return n
+
+if __name__ == "__main__":
+    a = int(input('Введи число для проверки на полигональность: '))
+    ln = lenA(a)
+    flag = True
+    while ln >= 2:
+        if Left(a, ln) == Right(a):
+            a = pos(a)
+            ln -= 1
+            a = per(a, ln)
+            ln -= 1
+        else:
+            flag = False
+            break
+    print(flag)
